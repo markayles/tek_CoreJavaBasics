1efb3f6fa580d69f7d5e79f027e95af5418a084f
Mark Ayles <marka2049@gmail.com>
2022-02-18 16:37:29 -0500

    Initial commit. Create problem 1

---
+++ src/com/teksystems/Main.java
@@ -0,0 +1,23 @@
+package com.teksystems;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Main m = new Main();
+
+        m.problem1();
+    }
+
+    public void problem1()
+    {
+        System.out.println("Problem 1");
+
+        int a = 1;
+        int b = 2;
+
+        int sum = a + b;
+
+        System.out.println("Sum " + sum);
+    }
+}
